38e59e79bf8bddc76b2eab766942fced81e19a44
ROHIT RANJAN KUMAR <rohitranjankumar@pec.edu>
2022-03-26 10:46:11 +0530

    Added solution - LeetHub

---
+++ 3. Get Element At Index - GFG/3.-get-element-at-index.cpp
@@ -0,0 +1,43 @@
+// { Driver Code Starts
+//Initial Template for C++
+#include <iostream>
+using namespace std;
+
+
+ // } Driver Code Ends
+//User function Template for C++
+
+//Complete this function
+int getByIndex(int arr[],int n,int i)
+{if(i>=0 && i<n)
+{
+    return arr[i];
+}
+else 
+return -1;
+    //Your code here
+}
+
+
+// { Driver Code Starts.
+
+int main() {
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	    int n;
+	    cin>>n;
+	    int arr[n];
+	    for(int i=0;i<n;i++)
+	    cin>>arr[i];
+	    
+	    int i;
+	    cin>>i;
+	    
+	    
+	    cout<<getByIndex(arr,n,i)<<endl;
+	    
+	}
+	return 0;
+}  // } Driver Code Ends
